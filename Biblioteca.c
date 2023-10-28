38a51dbae85a15000cdde1515008ccdc64210994
EduNunzi <148161063+EduNunzi@users.noreply.github.com>
2023-10-28 16:10:25 -0300

    Na Biblioteca.c, primeiro adicionamos as bibliotecas que temos em mente para as funções, e limitamos direto o número total de clientes em 1000 Usando a struct Cliente, e também limitamos as transações que aparecem no extrato em 100, com o struct Transação, e criamos int´s para usar de contadores nas funções.

--- Biblioteca.c
+++ Biblioteca.c
@@ -1,1 +1,8 @@
-#include "Biblioteca.h"
+#include <stdio.h>
+#include <string.h>
+#include "biblioteca.h"
+
+Cliente clientes[1000];
+int numClientes = 0;
+Transacao transacoes[100];
+int numTransacoes = 0;
