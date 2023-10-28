3be55706d9b80a0228ab4ec42c5163cb55ee37e9
lazzuriana08 <unifalazzuri@fei.edu.br>
2023-10-28 16:31:47 -0300

    A primeira função criada na "Biblioteca.c" é a função "Iniciar_Banco" que lê as informações das operações realizadas e estão salvas dentro do arquivo binário.

--- Biblioteca.c
+++ Biblioteca.c
@@ -6,3 +6,17 @@ Cliente clientes[1000];
 int numClientes = 0;
 Transacao transacoes[100];
 int numTransacoes = 0;
+
+void Iniciar_Banco() {
+    FILE* arquivo = fopen("Dados_do_Banco.bin", "rb");
+
+    if (arquivo != NULL) {
+        fread(&numClientes, sizeof(int), 1, arquivo);
+        fread(clientes, sizeof(Cliente), numClientes, arquivo);
+
+        fread(&numTransacoes, sizeof(int), 1, arquivo);
+        fread(transacoes, sizeof(Transacao), numTransacoes, arquivo);
+
+        fclose(arquivo);
+    }
+}
