9d6143f48cb39395c1ac149c34b612bacbfcf129
lazzuriana08 <unifalazzuri@fei.edu.br>
2023-10-28 16:37:00 -0300

    A função "Salvamento_Automatico()" foi criada com o intuito de salvar e atualizar automaticamente dentro do arquivo binário, as informaçãos dos clientes e das operações feitas.

--- Biblioteca.c
+++ Biblioteca.c
@@ -6,3 +6,32 @@ Cliente clientes[1000];
 int numClientes = 0;
 Transacao transacoes[100];
 int numTransacoes = 0;
+
+void Iniciar_Banco() {
+    FILE* arquivo = fopen("Dados_do_Banco.bin", "rb");
+
+    if (arquivo != NULL) {
+        fread(&numClientes, sizeof(int), 1, arquivo);
+        fread(clientes, sizeof(Cliente), numClientes, arquivo);
+
+        fread(&numTransacoes, sizeof(int), 1, arquivo);
+        fread(transacoes, sizeof(Transacao), numTransacoes, arquivo);
+
+        fclose(arquivo);
+    }
+}
+void Salvamento_Automatico() {
+    FILE* arquivo = fopen("Dados_do_Banco.bin", "wb");
+
+    if (arquivo != NULL) {
+
+        fwrite(&numClientes, sizeof(int), 1, arquivo);
+        fwrite(clientes, sizeof(Cliente), numClientes, arquivo);
+
+
+        fwrite(&numTransacoes, sizeof(int), 1, arquivo);
+        fwrite(transacoes, sizeof(Transacao), numTransacoes, arquivo);
+
+        fclose(arquivo);
+    }
+}
