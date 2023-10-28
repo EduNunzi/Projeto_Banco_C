392a48a73e05d78b7303ab1465dff8e6db4844b3
EduNunzi <148161063+EduNunzi@users.noreply.github.com>
2023-10-28 15:42:24 -0300

    Na Biblioteca.h, primeiro criamos a struct Cliente,com as variaveis (nome, cpf, tipo_conta, saldo, limite e senha) limitando os caracteres de nome em até 100, cpf em até 12, tipo_conta em até 5, saldo e limite são floats, e senha em até 7.

--- Biblioteca.h
+++ Biblioteca.h
@@ -1,4 +1,13 @@
 #ifndef PROJETO_BANCO_C_BIBLIOTECA_H
 #define PROJETO_BANCO_C_BIBLIOTECA_H
 
+typedef struct {
+    char nome[100];
+    char cpf[12];
+    char tipo_conta[5];
+    float saldo;
+    float limite;
+    char senha[7];
+} Cliente;
+
 #endif //PROJETO_BANCO_C_BIBLIOTECA_H
