887cf0d94d6d27a027954e7c9c8915a212a1cd57
lazzuriana08 <unifalazzuri@fei.edu.br>
2023-10-28 16:52:46 -0300

    Na função "Novo_Cliente", primeiro foi criada uma condicional "if" que limita o número de novos clientes criados para um máximo de 1000 clientes. Em seguida adicionamos as perguntas necessárias para que o cadatro seja feito. Logo após as perguntas, dentro das condicionais "if" e "else if", usamos a  função "strcmp" da bibloteca <string.h> para comparação de strings e identificar o tipo de conta (comum ou puls). Por fim um contador para adicionar o cliente e seus dado e a função de "Salvamento_Automatico chamando sua funcionalidade."

--- Biblioteca.c
+++ Biblioteca.c
@@ -6,3 +6,65 @@ Cliente clientes[1000];
 int numClientes = 0;
 Transacao transacoes[100];
 int numTransacoes = 0;
+
+void Iniciar_Banco() {
+    FILE* arquivo = fopen("Dados_do_Banco.bin", "rb");
+
+    if (arquivo != NULL) {
+        fread(&numClientes, sizeof(int), 1, arquivo);
+        fread(clientes, sizeof(Cliente), numClientes, arquivo);
+
+        fread(&numTransacoes, sizeof(int), 1, arquivo);
+        fread(transacoes, sizeof(Transacao), numTransacoes, arquivo);
+
+        fclose(arquivo);
+    }
+}
+
+void Salvamento_Automatico() {
+    FILE* arquivo = fopen("Dados_do_Banco.bin", "wb");
+
+    if (arquivo != NULL) {
+
+        fwrite(&numClientes, sizeof(int), 1, arquivo);
+        fwrite(clientes, sizeof(Cliente), numClientes, arquivo);
+
+
+        fwrite(&numTransacoes, sizeof(int), 1, arquivo);
+        fwrite(transacoes, sizeof(Transacao), numTransacoes, arquivo);
+
+        fclose(arquivo);
+    }
+}
+
+void Novo_Cliente() {
+    printf("----------------*Novo Cliente----------------*\n");
+    if (numClientes >= 1000) {
+        printf("Limite maximo de clientes atingido.\n");
+        return;
+    }
+
+    printf("Seu Nome: ");
+    scanf("%s", clientes[numClientes].nome);
+    printf("Seu CPF: ");
+    scanf("%s", clientes[numClientes].cpf);
+    printf("Tipo de conta (comum ou plus): ");
+    scanf(" %s", &clientes[numClientes].tipo_conta);
+    printf("Saldo inicial: ");
+    scanf("%f", &clientes[numClientes].saldo);
+
+    if (strcmp(clientes[numClientes].tipo_conta, "comum") == 0) {
+        clientes[numClientes].limite = -1000.0;
+    } else if (strcmp(clientes[numClientes].tipo_conta, "plus") == 0) {
+        clientes[numClientes].limite = -5000.0;
+    } else {
+        printf("Tipo de conta nao existente.\n");
+        return;
+    }
+    printf("Crie uma senha de 6 digitos: ");
+    scanf("%s", clientes[numClientes].senha);
+
+    numClientes++;
+
+    Salvamento_Automatico();
+}
