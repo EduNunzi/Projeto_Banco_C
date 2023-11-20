1995a72707204c25e5f134f0f8a272af8d37f98a
Ana Carolina Lazzuri <148160943+lazzuriana08@users.noreply.github.com>
2023-11-20 20:36:16 -0300

    Update Biblioteca.h
    Na biblioteca.h fizemos a adicação dos parâmetros necessários de cada função e também mudamos algumas funções que eram "void" para int. Essas modificações foram precisas para que as funções pudessem receber os parâmetros. Também criamos a função "clear_buffer" que ao ser chamada limpa o buffer do teclado e garante a bom funcionamento do código.

--- Biblioteca.h
+++ Biblioteca.h
@@ -18,14 +18,15 @@ typedef struct {
     float valor;
 } Transacao;
 
-void Iniciar_Banco();
-void Salvamento_Automatico();
-void Novo_Cliente();
-void Apagar_Cliente();
-void Lista_De_Clientes();
-void Funcao_Debito();
-void Funcao_Deposito();
-void Extrato_Clientes();
-void Transferencia_entre_Contas();
+void Iniciar_Banco(Cliente clientes[], int *numClientes, Transacao transacoes[], int *numTransacoes);
+int Salvamento_Automatico(Cliente clientes[], int numClientes, Transacao transacoes[], int numTransacoes);
+int Novo_Cliente(Cliente clientes[], int *numClientes);
+int Apagar_Cliente(Cliente clientes[], int *numClientes);
+int Lista_De_Clientes(Cliente clientes[], int numClientes);
+int Funcao_Debito(Cliente clientes[], int numClientes, Transacao transacoes[], int *numTransacoes);
+int Funcao_Deposito(Cliente clientes[], int numClientes, Transacao transacoes[], int *numTransacoes);
+int Extrato_Clientes(Cliente clientes[], int numClientes, Transacao transacoes[], int numTransacoes);
+int Transferencia_entre_Contas(Cliente clientes[], int numClientes, Transacao transacoes[], int *numTransacoes);
+void clear_buffer();
 
 #endif //PROJETO_BANCO_C_BIBLIOTECA_H
